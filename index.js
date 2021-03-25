f3437c3ed1917bdace97d6bf4eb2174550c0e679
adrianarafael2010@gmail.com <adrianarafael2010@gmail.com>
2021-03-25 10:56:26 -0300

    função lista

---
+++ index.js
@@ -0,0 +1,51 @@
+
+const nomePetshop = "PETSHOP AVANADE";
+let pets=[
+    { 
+    nome:'Pingo',
+    tipo: 'cachorro',
+    idade: 3,
+    raça: 'Pinscher',
+    peso: 3,
+    tutor: 'Eliane',
+    contato: '(81) 98378-7555',
+    vacinado:true,
+    serviços: ['banho','tosa']
+    
+},
+{ 
+    nome:'Chica',
+    tipo: 'coelho',
+    idade: 1,
+    raça: ' mini coelho Lion',
+    peso: 2,
+    tutor: 'David',
+    contato: '(81) 98850-7822',
+    vacinado:true,
+    serviços: ['banho','corte e unha']
+    
+},
+{ 
+    nome:'Apola',
+    tipo: 'cachorro',
+    idade: 6,
+    raça: 'Pastor Alemão',
+    peso: 9,
+    tutor: 'Manoel',
+    contato: '(81) 98817-6577',
+    vacinado:true,
+    serviços: ['banho']
+    
+}
+];
+
+ const listarPets = () => {
+     for(let i = 0 ; 1 > pets.length; i++)
+     {
+       console.log(pets[i].nome);
+     }
+    
+
+ }
+ listarPets();
+//console.log(pets);
